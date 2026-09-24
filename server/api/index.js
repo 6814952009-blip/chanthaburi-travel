d8435a9be6cd0e9365adddb9a85d948d9d02c1f4
6814952009-blip <6814952009@rbru.ac.th>
2026-09-24 11:25:46 +0700

    Move Vercel API under server

--- server/api/index.js
+++ server/api/index.js
@@ -1,5 +1,5 @@
-const app = require("../server/src/app");
-const connectDB = require("../server/src/config/db");
+const app = require("../src/app");
+const connectDB = require("../src/config/db");
 
 module.exports = async (req, res) => {
   try {
