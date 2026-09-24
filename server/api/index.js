a6a047fc6796221c52059c9fbf40ff3cdf690400
6814952009-blip <6814952009@rbru.ac.th>
2026-09-24 11:39:57 +0700

    Update deployment configuration

--- server/api/index.js
+++ server/api/index.js
@@ -2,11 +2,11 @@ const app = require("../src/app");
 const connectDB = require("../src/config/db");
 
 module.exports = async (req, res) => {
-  try {
-    await connectDB();
-    return app(req, res);
-  } catch (error) {
-    console.error(error);
-    return res.status(500).json({ message: "Database connection failed" });
-  }
+    try {
+        await connectDB();
+        return app(req, res);
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ message: "Database connection failed" });
+    }
 };
